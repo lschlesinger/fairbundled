388efda70c7c1f27791959efe265e679ea20d30f
Lorena Schlesinger <lorena.schlesinger@online.de>
2020-05-18 13:35:38 +0200

    new setup frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,35 @@
-import React, {Component} from 'react';
-import {BrowserRouter as Router, Route} from "react-router-dom";
-import 'antd/dist/antd.css';
-import UserRegistration from "./components/user-registration";
+import React from 'react';
+import logo from "./logo.jpg";
+import {BrowserRouter as Router, Link, Route} from "react-router-dom";
 
+import './App.css';
 
-class App extends Component {
-    render() {
-        return (
-            <Router>
-                <div className="container">
-                    <h2>FAIRBUNDLED Todo App</h2>
-                </div>
-                <Route path="/register" exact component={UserRegistration}/>
-            </Router>
-        );
-    }
+function App() {
+    return (
+        <Router>
+            <div className="container">
+
+                <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                    <a className="navbar-brand" href="https://codingthesmartway.com" target="_blank">
+                        <img src={logo} width="30" height="30" alt=""/>
+                    </a>
+                    <Link to="/" className="navbar-brand">MERN-Stack Todo App</Link>
+                    <div className="collpase nav-collapse">
+                        <ul className="navbar-nav mr-auto">
+                            <li className="navbar-item">
+                                <Link to="/" className="nav-link">Todos</Link>
+                            </li>
+                            <li className="navbar-item">
+                                <Link to="/create" className="nav-link">Create Todo</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+
+                <Route path="/register" exact component={Register}/>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
