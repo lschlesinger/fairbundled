89c0f7584e4f7dddd18ecea763fafe1f1e752371
Sebastian Nikles <sebastian.nikles@tum.de>
2020-06-25 15:31:28 +0200

    Shows the price levels

--- frontend/src/components/ProductDetails/CreateFairbundle.js
+++ frontend/src/components/ProductDetails/CreateFairbundle.js
@@ -1,7 +1,8 @@
 import React from "react";
-import {Typography, Space, Button, Radio, DatePicker} from "antd";
+import {Typography, Space, Button, Radio, DatePicker, Row, Col, Card} from "antd";
 
 const { Title, Text } = Typography;
+const style = { background: '#0092ff', padding: '8px 0' };
 
 export default class CreateFairbundle extends React.Component {
 
@@ -9,7 +10,8 @@ export default class CreateFairbundle extends React.Component {
         super(props);
 
         this.state = {
-            checkedOption:0,
+            checkedOption: 0,
+            selectedPriceLevel: 0,
             expirationDate: null
         };
     }
@@ -22,23 +24,46 @@ export default class CreateFairbundle extends React.Component {
 
     onDateChange = (date, dateString) => {
         this.setState({expirationDate: date});
-      }
+    }
+
+    onPriceLevelChange = (e) => {
+        this.setState({selectedPriceLevel: e.target.value});
+    }
+
+    getPriceLevel(priceLevel, index) {
+        let color;
+
+        if (index % 2) {
+            color = "#c8c8c8";
+        } else {
+            color = "#ffffff";
+        }
+
+        return (
+            <Col className="gutter-row" span={6}>
+                <Card style={{background:color, borderRadius:"12px", padding:"5px"}}>
+                    <Title level={3} style={{width:"100%", textAlign:"center", color:"#686868", fontWeight:"bold"}}>{priceLevel.unitPrice}€ / {priceLevel.unit}</Title>
+                    <Space style={{width:"100%", justifyContent:"center"}}><Text style={{color:"#686868"}}>bei einem Volumen von</Text></Space>
+                    <Space style={{width:"100%", justifyContent:"center", marginBottom:10}}><Text style={{color:"#686868", fontWeight:"bold"}}>{priceLevel.minQty} {priceLevel.unit}</Text></Space>
+                    <Space style={{width:"100%", justifyContent:"center"}}><Radio value={index}/></Space>
+                </Card>
+            </Col>
+        );
+    }
 
     render() {
         let product = this.props.product;
 
-        // let priceLevel = this.props.fairbundle.product.priceLevel.find(p => p.unitPrice === this.props.fairbundle.targetPrice);
-        // let date = Date.parse(this.props.fairbundle.expiration);
-        // let expirationAction = "trotzdem zum nächsthöheren Preis von bis zu " + this.props.fairbundle.product.priceLevel[0].unitPrice + "€ / " + this.props.fairbundle.product.priceLevel[0].unit + " ausgeführt.";
-
-        // if (this.props.fairbundle.expirationAction == "force") {
-        //     expirationAction = "abgebrochen und das Produkt wird nicht bestellt. Es entstehen keine Kosten.";
-        // }
-
         return (
             <div>
                 <Title level={2} style={{fontWeight:"bold"}}>Neues Fairbundle</Title>
+                <br />
                 
+                <Radio.Group size="large" onChange={this.onPriceLevelChange} value={this.state.selectedPriceLevel} style={{width:"100%", marginBottom:30}}>
+                    <Row gutter={[16, 16]}>
+                        {product.priceLevel.map((p, i) => this.getPriceLevel(p, i))}
+                    </Row>
+                </Radio.Group>
 
                 <br/>
                 <Text style={{marginRight:"10px", fontSize:18, color:"#454545"}}>Zieldatum:</Text>
