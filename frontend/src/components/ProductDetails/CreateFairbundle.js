188465e1aaac4be1d2a63b04aa633c1aedac0818
Sebastian Nikles <sebastian.nikles@tum.de>
2020-06-25 15:11:04 +0200

    Shows datepicker and expiration action in the dialog

--- frontend/src/components/ProductDetails/CreateFairbundle.js
+++ frontend/src/components/ProductDetails/CreateFairbundle.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Card, Typography, Space, Button} from "antd";
+import {Typography, Space, Button, Radio, DatePicker} from "antd";
 
 const { Title, Text } = Typography;
 
@@ -7,6 +7,11 @@ export default class CreateFairbundle extends React.Component {
 
     constructor(props) {
         super(props);
+
+        this.state = {
+            checkedOption:0,
+            expirationDate: null
+        };
     }
 
     onCreate = () => {
@@ -14,6 +19,10 @@ export default class CreateFairbundle extends React.Component {
 
         this.props.createFairbundle();
     };
+
+    onDateChange = (date, dateString) => {
+        this.setState({expirationDate: date});
+      }
 
     render() {
         let product = this.props.product;
@@ -30,6 +39,22 @@ export default class CreateFairbundle extends React.Component {
             <div>
                 <Title level={2} style={{fontWeight:"bold"}}>Neues Fairbundle</Title>
                 
+
+                <br/>
+                <Text style={{marginRight:"10px", fontSize:18, color:"#454545"}}>Zieldatum:</Text>
+                <DatePicker onChange={this.onDateChange} placeholder="Datum wählen" format={'DD.MM.YYYY'} style={{fontSize:18, fontWeight:"bold", color:"#686868", marginBottom:30, width:"15%"}} />
+
+                <Space style={{width:"100%", marginBottom:10}}><Text style={{fontSize:18, color:"#454545"}}>Bei Nichterreichen des Zielpreises soll</Text></Space>
+                <Radio.Group onChange={e => {this.setState({checkedOption:e.target.value})}} value={this.state.checkedOption}>
+                    <Radio value={0} style={{width:"100%"}}>
+                        <Text style={{fontSize:18, color:"#454545"}}>das Fairbundle trotzdem zum nächsthöheren Preis ausgeführt werden</Text>
+                    </Radio>
+                    <Radio value={1}>
+                        <Text style={{fontSize:18, color:"#454545"}}>das Fairbundle abgebrochen werden</Text>
+                    </Radio>
+                </Radio.Group>
+                <br/>
+                <br/>
                 <Space style={{width:"100%", justifyContent:"flex-end"}}>
                     <Button type="primary" style={{width:"240px", height:"40px"}} ref={(buttonDOM) => { this.createDOM = buttonDOM; }} onClick={this.onCreate}>
                         <Text style={{color:"#ffffff", fontSize:18, fontWeight:"bold"}}>
@@ -49,13 +74,6 @@ export default class CreateFairbundle extends React.Component {
                     <Space style={{width:"100%", justifyContent:"center"}}><Text style={{color:"#686868"}}>bei einem Volumen von</Text></Space>
                     <Space style={{width:"100%", justifyContent:"center"}}><Text style={{color:"#686868", fontWeight:"bold"}}>{priceLevel.minQty} {priceLevel.unit}</Text></Space>
                 </Card>
-                <br/>
-                <Text style={{marginRight:"10px", fontSize:18, color:"#454545"}}>Zieldatum:</Text>
-                <Text style={{fontSize:18, fontWeight:"bold", color:"#686868"}}>{this.formatDate(date)}</Text>
-                <br/>
-                <br/>
-                <Text style={{fontSize:18, color:"#454545"}}>Bei Nichterreichen des Zielpreises wird das Fairbundle </Text>
-                <Text style={{fontSize:18, color:"#454545", fontWeight:"bold"}}>{expirationAction}</Text>
-                <br/>
-                <br/>
+                
+                
                 */
