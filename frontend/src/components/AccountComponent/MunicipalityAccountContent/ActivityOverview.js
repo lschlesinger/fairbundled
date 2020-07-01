70909b3a61460eb67a5f289952dce273b38a4807
Lorena Schlesinger <lorena.schlesinger@online.de>
2020-07-01 08:20:55 +0200

    Refactor with services MunicipalityAccountView.js and CheckoutView.js

--- frontend/src/components/AccountComponent/MunicipalityAccountContent/ActivityOverview.js
+++ frontend/src/components/AccountComponent/MunicipalityAccountContent/ActivityOverview.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import {Card, Col, Row, Table, Tabs, Typography} from "antd";
+import {Card, Row, Table, Tabs, Typography} from "antd";
+import OverviewBanner from "./OverviewBanner";
 
 const {Text, Title} = Typography;
 const {TabPane} = Tabs;
@@ -12,179 +13,6 @@ export default class ActivityOverview extends React.Component {
             activeTab: "all"
         };
 
-    }
-
-    handleChange = (key) => {
-        this.setState({
-            activeTab: key
-        })
-    };
-
-    renderTabProductCount() {
-        let productCount = 0;
-        if (this.state.activeTab === 'all') {
-            productCount = this.props.municipality.fairbundleProductsBought.length + this.props.municipality.directProductsBought.length;
-        } else if (this.state.activeTab === 'single') {
-            productCount = this.props.municipality.directProductsBought.length;
-        } else if (this.state.activeTab === 'fairbundle') {
-            productCount = this.props.municipality.fairbundleProductsBought.length;
-        }
-        return productCount;
-    }
-
-    renderTabOrderCount() {
-        let orderCount = 0;
-        if (this.state.activeTab === 'all') {
-            orderCount = this.props.municipality.fairbundlesSubmitted.length + this.props.municipality.directOrdersSubmitted.length;
-        } else if (this.state.activeTab === 'single') {
-            orderCount = this.props.municipality.directOrdersSubmitted.length;
-        } else if (this.state.activeTab === 'fairbundle') {
-            orderCount = this.props.municipality.fairbundlesSubmitted.length;
-        }
-        return orderCount;
-    }
-
-    renderTabPositionsValue() {
-        let positionsValue = 0;
-        if (this.state.activeTab === 'all') {
-            positionsValue = this.props.municipality.fairbundleSpendings + this.props.municipality.directOrderSpendings;
-        } else if (this.state.activeTab === 'single') {
-            positionsValue = this.props.municipality.directOrderSpendings;
-        } else if (this.state.activeTab === 'fairbundle') {
-            positionsValue = this.props.municipality.fairbundleSpendings;
-        }
-        return positionsValue;
-    }
-
-
-    renderCustomizedOverview() {
-        return (
-            <Row className="margin--sm order-overview__kpis" gutter={8}>
-                <Col span={7}>
-                    <Row className="padding-top--sm" justify="center">
-                        <Text className="order-overview__number" strong>
-                            {this.renderTabProductCount()}
-                        </Text>
-                    </Row>
-                    <Row
-                        justify="center"
-                        className="padding-top--xs padding-bottom--sm"
-                    >
-                        <Text strong>Produkte</Text>
-                    </Row>
-                </Col>
-                <Col span={7}>
-                    <Row className="padding-top--sm" justify="center">
-                        <Text className="order-overview__number" strong>
-                            {this.renderTabOrderCount()}
-                        </Text>
-                    </Row>
-                    <Row
-                        justify="center"
-                        className="padding-top--xs padding-bottom--sm"
-                    >
-                        <Text strong>Bestellungen</Text>
-                    </Row>
-                </Col>
-                <Col span={10}>
-                    <Row className="padding-top--sm" justify="center">
-                        <Text className="order-overview__number" strong>
-                            {new Intl.NumberFormat("de-DE", {
-                                style: "currency",
-                                currency: "EUR",
-                            }).format(this.renderTabPositionsValue())}
-                        </Text>
-                    </Row>
-                    <Row
-                        justify="center"
-                        className="padding-top--xs padding-bottom--sm"
-                    >
-                        <Text strong>Gesamtausgaben</Text>
-                    </Row>
-                </Col>
-            </Row>);
-    }
-
-    render() {
-        return (
-            <Card title="Meine Aktivitäten">
-                {/* Bestellungen (normale und Fairbundle) der letzten 30 oder 60 Tage */}
-                <Row className="margin-vertical--sm">
-                    <Title className="padding-right--sm" level={4}>
-                        Übersicht
-                    </Title>
-                </Row>
-                <Tabs defaultActiveKey="all" onChange={this.handleChange}>
-                    <TabPane tab="Gesamt" key="all">
-                        {this.renderCustomizedOverview()}
-                    </TabPane>
-                    <TabPane tab="Fairbundle" key="fairbundle">
-                        {this.renderCustomizedOverview()}
-                    </TabPane>
-                    <TabPane tab="Direktbestellung" key="single">
-                        {this.renderCustomizedOverview()}
-                    </TabPane>
-                </Tabs>
-                <Row className="margin-bottom--sm margin-top--md">
-                    <Title className="padding-right--sm" level={4}>
-                        Laufende Fairbundle Bestellungen
-                    </Title>
-                </Row>
-                <Row>
-                    <Table columns={this.getFairbundleTableColumns()}
-                           pagination={{size:"small"}}
-                           dataSource={this.getFairbundleTableData()}>
-                    </Table>
-                </Row>
-                <Row className="margin-bottom--sm margin-top--md">
-                    <Title className="padding-right--sm" level={4}>
-                        Abgeschlossene Bestellungen
-                    </Title>
-                </Row>
-                <Row>
-                    <Table columns={this.getOrderTableColumns()}
-                           dataSource={this.getOrderTableData()}
-                           pagination={{size:"small"}}
-                           expandable={{
-                               expandedRowRender: record => this.renderPositionRecords(record),
-                               rowExpandable: record => record.type === ('Direktbestellung')
-                           }}>
-                    </Table>
-                </Row>
-            </Card>
-        );
-    }
-
-    getFairbundleTableColumns() {
-        return [
-            {
-                title: 'Produkt',
-                dataIndex: 'product',
-                key: 'product'
-            },
-            {
-                title: 'Menge',
-                dataIndex: 'qty',
-                key: 'qty'
-            },
-            {
-                title: 'Zielpreis',
-                dataIndex: 'price',
-                key: 'price'
-            },
-            {
-                title: 'Laufzeitende',
-                dataIndex: 'expiration',
-                key: 'expiration',
-                defaultSortOrder: 'descend',
-                sorter: (a, b) => a.rawExpiration - b.rawExpiration
-            },
-            {
-                title: 'Status',
-                dataIndex: 'status',
-                key: 'status'
-            }
-        ];
     }
 
     getOrderTableData() {
@@ -212,7 +40,6 @@ export default class ActivityOverview extends React.Component {
             };
             data.push(entry);
         });
-        fairbundlesSubmitted = this.props.municipality.fairbundlesSubmitted;
         fairbundlesSubmitted.map((order) => {
             let positions = this.props.municipality.fairbundlesSubmittedPositions.filter((position) => position.order._id.toString() === order._id.toString());
             let name = positions[0].product.name;
@@ -236,51 +63,6 @@ export default class ActivityOverview extends React.Component {
             data.push(entry);
         });
         return data;
-    }
-
-    getOrderTableColumns() {
-        return [
-            {
-                title: 'Typ',
-                dataIndex: 'type',
-                key: 'type',
-                filters: [
-                    {
-                        text: 'Fairbundle',
-                        value: 'Fairbundle'
-                    },
-                    {
-                        text: 'Direktbestellung',
-                        value: 'Direktbestellung'
-                    }
-                ],
-                onFilter: (value, record) => record.type.indexOf(value) === 0,
-            },
-            {
-                title: 'Bestellnummer',
-                dataIndex: 'order',
-                key: 'order'
-            },
-            {
-                title: 'Positionen',
-                dataIndex: 'positions',
-                key: 'positions'
-            },
-            {
-                title: 'Gesamtpreis',
-                dataIndex: 'price',
-                key: 'price',
-                defaultSortOrder: 'descend',
-                sorter: (a, b) => a.rawPrice - b.rawPrice
-            },
-            {
-                title: 'Datum',
-                dataIndex: 'submission',
-                key: 'submission',
-                defaultSortOrder: 'descend',
-                sorter: (a, b) => a.rawSubmission - b.rawSubmission
-            }
-        ];
     }
 
     getFairbundleTableData() {
@@ -311,32 +93,12 @@ export default class ActivityOverview extends React.Component {
 
     renderPositionRecords(record) {
         return (
-            <Table columns={this.getSubTableColumns()}
+            <Table columns={this.props.positionSubTableColumns}
                    dataSource={this.getSubTableData(record)}
                    pagination={false}
                    className="padding--md">
             </Table>
         )
-    }
-
-    getSubTableColumns() {
-        return [
-            {
-                title: 'Produkt',
-                dataIndex: 'product',
-                key: 'product'
-            },
-            {
-                title: 'Menge',
-                dataIndex: 'qty',
-                key: 'qty'
-            },
-            {
-                title: 'Preis',
-                dataIndex: 'price',
-                key: 'price'
-            }
-        ];
     }
 
     getSubTableData(record) {
@@ -360,5 +122,42 @@ export default class ActivityOverview extends React.Component {
             data.push(entry);
         });
         return data;
+    }
+
+
+    render() {
+        return (
+            <Card title="Meine Aktivitäten">
+                {/* Übersichtsbanner (normale und Fairbundle*/}
+                <OverviewBanner municipality={this.props.municipality}/>
+                <Row className="margin-bottom--sm margin-top--md">
+                    <Title className="padding-right--sm" level={4}>
+                        Laufende Fairbundle Bestellungen
+                    </Title>
+                </Row>
+                <Row>
+                    <Table columns={this.props.fairbundleTableColumns}
+                           pagination={{size:"small"}}
+                           dataSource={this.getFairbundleTableData()}>
+                    </Table>
+                </Row>
+                <Row className="margin-vertical--sm">
+                    <Title className="padding-right--sm" level={4}>
+                        Abgeschlossene Bestellungen
+                    </Title>
+                </Row>
+                <Row>
+                    <Table columns={this.props.orderTableColums}
+                           dataSource={this.getOrderTableData()}
+                           pagination={{size: "small"}}
+                           expandable={{
+                               expandedRowRender: record => this.renderPositionRecords(record),
+                               rowExpandable: record => record.type === ('Direktbestellung')
+                           }}>
+                    </Table>
+                </Row>
+
+            </Card>
+        );
     }
 }
